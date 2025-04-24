8e0ecb4aed4f084e1e1f994a74f10c5d78dc56a2
Nikita Panwar <nikitapanwar987@gmail.com>
2025-04-25 04:13:54 +0530

    Added Dark theme

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,13 +1,12 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+// tailwind.config.js
+module.exports = {
+  darkMode: 'class', // Enables dark mode via 'class' strategy
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
- 
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
